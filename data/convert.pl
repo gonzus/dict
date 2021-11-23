c56aca8889ff547ad4ab7f36e3ff8b55971056e0
Gonzalo Diethelm <gonzalo.diethelm@gmail.com>
2021-11-23 13:34:57 +0100

    Make convert script more intelligent

--- data/convert.pl
+++ data/convert.pl
@@ -1,10 +1,26 @@
 use strict;
 use warnings;
 
+use Path::Tiny;
+use Getopt::Long  qw/ GetOptions /;
+
+my %OPTS = (
+    cat   => '',
+    part  => 'noun',
+    guess => 1,
+    help  => 0,
+);
+Getopt::Long::GetOptions(
+    'guess'  => \$OPTS{guess},
+    'cat=s'  => \$OPTS{cat},
+    'part=s' => \$OPTS{part},
+    'help'  => \$OPTS{help},
+);
+
 exit main();
 
 sub process_file {
-    my ($name, $cat, $part) = @_;
+    my ($name) = @_;
     # printf("File [%s]\n", $name);
 
     my $count = 0;
@@ -14,18 +30,48 @@ sub process_file {
         chomp($line);
         # printf("%3d | [%s]\n", $count, $line);
         my @cols = split(' ', $line);
+
+        my $cat = '';
+        if ($OPTS{cat}) {
+            $cat = $OPTS{cat};
+        } elsif ($OPTS{guess}) {
+            my $p = path($name);
+            my $b = $p->basename;
+            $cat = $b =~ s:^[-_0-9]+|\.[-_a-zA-Z0-9]+$::rg;
+        }
+        $cat = "-c $cat" if $cat;
+
+        my $part = $OPTS{part};
+
         my $en = sprintf("%s:%s"   , 'en', $cols[2]);
         my $nl = sprintf("%s:%s+%s", 'nl', $cols[1], $cols[0]);
         my $es = sprintf("%s:%s+%s", 'es', $cols[4], $cols[3]);
-        printf("add -c %s %s %-30.30s %-30.30s %s\n",
+
+        printf("add %s %s %-30.30s %-30.30s %s\n",
                $cat, $part, $en, $nl, $es);
     }
     close $fp;
 }
 
+sub show_help {
+    print <<EOS;
+Convert data from Google Sheet to dict commands
+Usage: $0 [options]
+    --cat <category>  category to use, empty for none (default is "$OPTS{cat}")
+    --part <part>     part to use (default is "$OPTS{part}")
+    --guess           guess category from file name (default is $OPTS{guess})
+    --help            print this help
+EOS
+}
+
 sub main {
+    if ($OPTS{help}) {
+        show_help();
+        return 0;
+    }
+
     foreach my $arg (@ARGV) {
-        process_file($arg, 'clothing', 'noun');
+        process_file($arg);
     }
     return 0;
 }
