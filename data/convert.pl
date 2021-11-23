70b92d61e9f8521e67dadbbadd7247f699688ee2
Gonzalo Diethelm <gonzalo.diethelm@gmail.com>
2021-11-23 15:23:32 +0100

    More data: people

--- data/convert.pl
+++ data/convert.pl
@@ -44,23 +44,40 @@ sub process_file {
         }
         $cat = "-c $cat" if $cat;
 
+        my $en = '';
+        my $nl = '';
+        my $es = '';
+
         if ($part eq 'noun') {
-            my $en = sprintf("%s:%s"   , 'en', $cols[2]);
-            my $nl = sprintf("%s:%s+%s", 'nl', $cols[1], $cols[0]);
-            my $es = sprintf("%s:%s+%s", 'es', $cols[4], $cols[3]);
-            printf("add %s %s %-30.30s %-30.30s %s\n",
-                $cat, $part, $en, $nl, $es);
-            next;
+            if (@cols >= 2) {
+                $en = sprintf("%s:%s"   , 'en', $cols[2]);
+            }
+            if (@cols >= 1) {
+                $nl = sprintf("%s:%s", 'nl', $cols[1]);
+                $nl .= sprintf("+%s", $cols[0]) unless $cols[0] eq '?';
+            }
+            if (@cols >= 4) {
+                $es = sprintf("%s:%s", 'es', $cols[4]);
+                $es .= sprintf("+%s", $cols[3]) unless $cols[3] eq '?';
+            }
         }
 
         if ($part eq 'adjective') {
-            my $en = sprintf("%s:%s", 'en', $cols[1]);
-            my $nl = sprintf("%s:%s", 'nl', $cols[0]);
-            my $es = sprintf("%s:%s", 'es', $cols[2]);
-            printf("add %s %s %-30.30s %-30.30s %s\n",
-                $cat, $part, $en, $nl, $es);
-            next;
+            if (@cols >= 1) {
+                $en = sprintf("%s:%s", 'en', $cols[1]);
+            }
+            if (@cols >= 0) {
+                $nl = sprintf("%s:%s", 'nl', $cols[0]);
+            }
+            if (@cols >= 2) {
+                $es = sprintf("%s:%s", 'es', $cols[2]);
+            }
         }
+
+        next unless $en || $nl || $es;
+
+        my $out = sprintf("add %s %s %-30.30s %-30.30s %s\n", $cat, $part, $en, $nl, $es);
+        printf("%s\n", $out =~ s/\s*$//grs);
     }
     close $fp;
 }
